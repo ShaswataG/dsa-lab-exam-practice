b5afa2195fc8941444ef97405868e98f89b38096
ShaswataG <121812904+ShaswataG@users.noreply.github.com>
2023-11-29 23:39:10 +0530

    merge sort done

--- 08.Heap sort and Merge sort.c
+++ 08.Heap sort and Merge sort.c
@@ -42,6 +42,35 @@ void heapSort(int a[], int n) {
     }
 }
 
+void merge(int a[], int l, int mid, int h) {
+    int k = l;
+    int i = l, j = mid+1;
+    int b[h+1];
+    while (i <= mid && j <= h) {
+        if (a[i] < a[j]) {
+            b[k++] = a[i++];
+        } else {
+            b[k++] = a[j++];
+        }
+    }
+    while (i <= mid) {
+        b[k++] = a[i++];
+    }
+    while (j <= h) {
+        b[k++] = a[j++];
+    }
+    for (int i = l; i <= h; i++) a[i] = b[i];
+}
+
+void mergeSort(int a[], int l, int h) {
+    if (l < h) {
+        int mid = (l+h)/2;
+        mergeSort(a, l, mid);
+        mergeSort(a, mid+1, h);
+        merge(a, l, mid, h);
+    }
+}
+
 void display(int a[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
@@ -52,7 +81,8 @@ void display(int a[], int n) {
 int main() {
     int a[] = {6, 3, 4, 9, 10, 11, 9}, n = 7;
     display(a, n);
-    heapSort(a, n);
+    // heapSort(a, n);
+    mergeSort(a, 0, 6);
     display(a, n);
     return 0;
 }
